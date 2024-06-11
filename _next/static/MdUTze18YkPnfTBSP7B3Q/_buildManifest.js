58df3096b76cc4b18d8d8e7eadcad230a2efc648
bslgrgr <bslgrgr@users.noreply.github.com>
2024-06-11 06:02:57 +0000

    deploy: 5be47edebfad84bf134556612c535962c4e196a1

---
+++ _next/static/MdUTze18YkPnfTBSP7B3Q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-face9d1f4c81f9fe.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
